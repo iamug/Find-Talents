241461dda75136f0a15318142c4219b6b02518cf
Aguziendu Ugochukwu <uaguziendu@gmail.com>
2022-03-19 18:23:36 +0100

    minor bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ function App(props: any) {
           <Switch>
             <Layout>
               <Switch>
-                <Route path={`${props.match.url}home`} exact component={Home} />
-                <Route path={`${props.match.url}saved`} exact component={Saved} />
+                <Route path="home" exact component={Home} />
+                <Route path="saved" exact component={Saved} />
                 <Route component={Home} />
               </Switch>
             </Layout>
